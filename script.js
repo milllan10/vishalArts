ce6467c7e0595fc4c1f17e7f6e0109c38f1bbae0
Milan Hingu <milanhingu9987@gmail.com>
2024-03-24 14:10:48 +0530

    added price for pensil sketch

---
+++ script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+	$("#mobile-menu-toggle").click(function () {
+		$("#mobile-menu").animate({ right: "0" }, 200);
+		$("#mobile-menu-toggle").hide(); // Hide menu button
+		$("#close-btn").show(); // Show close button
+	});
+
+	$(document).on("click", "#close-btn", function () {
+		$("#mobile-menu").animate({ right: "-200px" }, 200);
+		$("#close-btn").hide(); // Hide close button
+		$("#mobile-menu-toggle").show(); // Show menu button
+  	});
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+$(".bandwsection, .colorsection").on("click", function () {
+  var allITags = $(
+    ".bandwsection i.fa-chevron-down, .colorsection i.fa-chevron-down"
+  );
+  $(".bandwprice").not($(this).siblings(".bandwprice")).slideUp();
+  $(this).siblings(".bandwprice").slideToggle();
+  var nearestITag = $(this).find("i.fa-chevron-down");
+  if (nearestITag.length > 0) {
+    nearestITag.addClass("rotatearrow");
+    setTimeout(function () {
+      nearestITag.toggleClass("rotate180");
+    }, 10);
+  }
+  allITags.not(nearestITag).each(function () {
+    $(this).removeClass("rotatearrow rotate180");
+  });
+});
